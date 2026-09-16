8f8fae87967a5093406da088bd65541691086517
MAKSjunaid <134540767+MAKSjunaid@users.noreply.github.com>
2026-09-16 17:49:50 +0530

    Update TradeLogEntry.java

--- src/main/java/com/tradesignal/model/TradeLogEntry.java
+++ src/main/java/com/tradesignal/model/TradeLogEntry.java
@@ -5,7 +5,7 @@ public class TradeLogEntry {
     public String mode;
     public double entryPrice;
     public int qty;
-    /** Money Actually put to work on this order: entryPrice * qty. */
+    /** Money actually put to work on this order: entryPrice * qty. */
     public double investedAmount;
     public String entryTime;
     public double exitPrice;
